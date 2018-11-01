7f7e30dcf0b0e91460dd02924854cd257f71176f
Chau Ngoc Minh Nguyen <tug37553@chinook.cis.temple.edu>
2018-11-01 14:47:10 -0400

    Pearl had added functions that read the txt files and scan the matrix sizes. Next is Allocate the memomry an create matrixs. I had tested my functions on my PC but couldn't test on chinook. You two can have a look and let me know what you think

--- mmult_mpi_omp.c
+++ mmult_mpi_omp.c
@@ -29,14 +29,58 @@ int main(int argc, char* argv[])
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
   MPI_Comm_rank(MPI_COMM_WORLD, &myid);
-  if (argc > 1) {
-    nrows = atoi(argv[1]);
-    ncols = nrows;
+
+
+
+  //Arg must more than 2
+  if (argc > 2) {
+    //nrows = atoi(argv[1]);
+    //ncols = nrows;
     if (myid == 0) {
       // Master Code goes here
-      aa = gen_matrix(nrows, ncols);
-      bb = gen_matrix(ncols, nrows);
-      cc1 = malloc(sizeof(double) * nrows * nrows); 
+
+	//Here is what I have for reading the size of 2 array
+	FILE * file1;
+	FILE * file2;
+	char line[100];
+	int row1, col1, row2, col2;
+	file1=fopen(argv[1],"r");
+	file2=fopen(argv[2],"r");
+	//If one of the file is NULL, exit
+	//However, I beleive wee need to send a message to the slaves to terminate
+	//Have to do with MPI_Send
+	if(file1==NULL||file2==NULL){
+		printf("Fopen failed\n");
+		return 1;
+	}
+	//open the first file and get the number of row and collumn for first file
+	if(fgets(line, sizeof line, file1)){
+		//get row
+		char *token = strtok(line, "rows()cols ");
+		row1=atoi(token);
+		//get col
+		token = strtok(NULL, "rows()cols ");
+		col1 =atoi(token);
+	}
+	printf("\nRow: %d by Column %d\n", row1,col1);
+	fclose(file1);
+
+	//Read the file 2 and get row and col of second matrix 
+	if(fgets(line, sizeof line, file2)){
+                //get row2
+                char *token = strtok(line, "rows()cols ");
+                row2=atoi(token);
+                //get col2
+                token = strtok(NULL, "rows()cols ");
+                col2 =atoi(token);
+        }
+        printf("\nRow2 : %d by Column2 %d\n", row2,col2);
+	fclose(file2);
+
+      //aa = gen_matrix(nrows, ncols);
+      //bb = gen_matrix(ncols, nrows);
+     // printf("%f",*aa);
+      //cc1 = malloc(sizeof(double) * nrows * nrows); 
       starttime = MPI_Wtime();
       /* Insert your master code here to store the product into cc1 */
       endtime = MPI_Wtime();
